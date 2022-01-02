db226b00bfb6ac869731086c84ad50e81b539ae6
David Gati <gatidavid2012@gmail.com>
2022-01-03 01:37:48 +0300

    Singleton Class using Synchronized Block

---
+++ src/com/dave/singleton/PrinterSingletonSynBlock.java
@@ -0,0 +1,18 @@
+package com.dave.singleton;
+
+public class PrinterSingletonSynBlock {
+
+	private static PrinterSingletonSynBlock INSTANCE;
+	
+	private PrinterSingletonSynBlock() {
+		
+	}
+	
+	public static PrinterSingletonSynBlock getInstance() {
+		synchronized (PrinterSingletonSynBlock.class) {
+			if(INSTANCE== null)
+				INSTANCE = new PrinterSingletonSynBlock();	
+		}
+		return INSTANCE;
+	}
+}
--- src/com/dave/test/TestApp.java
+++ src/com/dave/test/TestApp.java
@@ -1,5 +1,6 @@
 package com.dave.test;
 
+import com.dave.singleton.PrinterSingletonSynBlock;
 import com.dave.singleton.PrinterSynchronizedMethod;
 
 public class TestApp {
@@ -11,13 +12,19 @@ public class TestApp {
 		
 		System.out.println("hash Codes: "+print1.hashCode()+" "+print2.hashCode());
 		System.out.println("print1=print2? "+(print1==print2));
-		*/
+		
 		
 		PrinterSynchronizedMethod print1 = PrinterSynchronizedMethod.getInstance();
 		PrinterSynchronizedMethod print2= PrinterSynchronizedMethod.getInstance();
 		
 		System.out.println("hash Codes: "+print1.hashCode()+" "+print2.hashCode());
 		System.out.println("print1=print2? "+(print1==print2));
+		*/
 		
+		PrinterSingletonSynBlock print1 = PrinterSingletonSynBlock.getInstance();
+		PrinterSingletonSynBlock print2 = PrinterSingletonSynBlock.getInstance();
+		
+		System.out.println("hash Codes: "+print1.hashCode()+" "+print2.hashCode());
+		System.out.println("print1=print2? "+(print1==print2));
 	}
 }
